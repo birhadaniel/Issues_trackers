4ae8f0b51154faab89415f4953629868378c7fe2
Daniel Birhanu <danielbirhanu0374@gmail.com>
2025-10-08 19:34:08 +0300

    Refactor:Organize import statement

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,5 @@
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes'; 
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import { Skeleton } from '@/app/components';
+import { Box, Card, Flex } from '@radix-ui/themes'; 
 const LoadingIssueDetailPage = () => {
   return (
     <Box className='max-w-xl'>
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { Table } from '@radix-ui/themes';
+import { Skeleton } from '@/app/components';
 import IssueActions from './IssueActions';
 
 const LoadingIssuesPage = () => {
